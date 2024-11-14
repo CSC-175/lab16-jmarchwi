1e65c78e1bf51db002ff85b347cee5a5a1839af1
James Marchwinski <jmarchwi@umich.edu>
2024-11-14 12:45:42 -0500

    First attempt

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,19 @@
-// Code to implement the gcf function goes here
+int gcf(int a, int b) {
+    int x, y, r;
+    if (a > b){
+        x = a;
+        y = b;
+    }
+    else if (a < b) {
+        x = b;
+        y = a;
+    }
+    else{return a;}
+    r = x % y;
+    if(r==0){return y;}
+    else {
+        a = y;
+        b = r;
+        gcf(a,b);
+    }
+}
